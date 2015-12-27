bf88e4d4503e559e52198f4128691fceb5372ade
Fedor Gogolev <knsd@knsd.net>
2015-12-27 05:34:51 +0300

    Read first two opcodes

--- src/opcode.rs
+++ src/opcode.rs
@@ -90,3 +90,58 @@ pub enum OpCode {
     PersId(Vec<u8>),
     BinPersId,
 }
+
+pub fn read_opcode<R>(rd: &mut R) -> Result<OpCode, Error> where R: Read + BufRead {
+    let marker = try!(rd.read_u8());
+    return Ok(match marker {
+        73 => {
+            let mut buf = Vec::new();
+            try!(rd.read_until('\n' as u8, &mut buf));
+
+            // Skip last symbol — \n
+            let init = match buf.split_last() {
+                None => return Err(Error::InvalidString),
+                Some((_last, init)) => init,
+            };
+
+            OpCode::Int(try!(try!(from_utf8(init)).parse()))
+        },
+        74 => OpCode::BinInt(try!(rd.read_i32::<LittleEndian>())),
+        c => return Err(Error::UnknownOpcode(c)),
+    })
+}
+
+#[cfg(test)]
+mod tests {
+    use std::io::{Cursor};
+
+    use num::{FromPrimitive};
+
+    use super::{OpCode, Error, read_opcode};
+
+    macro_rules! t {
+        ($buffer: expr, $pat:pat, $result:expr) => ({
+            match read_opcode(&mut Cursor::new(&$buffer[..])) {
+                $pat => $result,
+                other => {
+                    println!("ERROR {:?}", other);
+                    assert!(false)
+                },
+            }
+        })
+    }
+
+    #[test]
+    fn test_int() {
+        t!(b"I", Err(Error::InvalidString), assert!(true));
+        t!(b"I\n", Err(Error::InvalidInt), assert!(true));
+        t!(b"Iabc\n", Err(Error::InvalidInt), assert!(true));
+        t!(b"I123\n", Ok(OpCode::Int(n)), assert_eq!(n, FromPrimitive::from_usize(123).unwrap()));
+    }
+
+    #[test]
+    fn test_bin_int() {
+        t!(b"J\x0a", Err(Error::ReadError(_)), assert!(true));
+        t!(b"J\x0a\x00\x00\x00", Ok(OpCode::BinInt(n)), assert_eq!(n, 10));
+    }
+}
