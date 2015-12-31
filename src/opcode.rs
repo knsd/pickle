526e68f5d6a6eaaf053813f98c9c77e6bca80f14
Fedor Gogolev <knsd@knsd.net>
2015-12-31 04:15:52 +0300

    Add string unescape function

--- src/opcode.rs
+++ src/opcode.rs
@@ -2,6 +2,7 @@ use std::io::{Read, BufRead, Error as IoError, ErrorKind};
 use std::str::{from_utf8, Utf8Error};
 use std::string::{FromUtf8Error};
 use std::num::{ParseIntError, ParseFloatError};
+use std::collections::{VecDeque};
 
 use num::{Zero};
 use num::bigint::{BigInt, ToBigInt, Sign, ParseBigIntError};
@@ -168,6 +169,105 @@ fn read_long<R>(rd: &mut R, length: usize) -> Result<BigInt, Error> where R: Rea
     Ok(n)
 }
 
+fn hex_to_digit(c: u8) -> Result<u8, &'static str> {
+    Ok(match c {
+        b'0' ... b'9' => c - b'0',
+        b'a' ... b'f' => c - b'a' + 10,
+        b'A' ... b'F' => c - b'A' + 10,
+        _ => return Err("Invalid hex value"),
+    })
+}
+
+fn oct_to_digit(c: u8) -> Result<u8, &'static str> {
+    Ok(match c {
+        b'0' ... b'7' => c - b'0',
+        _ => return Err("Invalid oct value"),
+    })
+}
+
+fn unescape(s: &[u8]) -> Result<Vec<u8>, &'static str> {
+    let mut buf = Vec::with_capacity(s.len());
+    let mut oct_buf = VecDeque::with_capacity(3);
+
+    let mut i = 0;
+
+    macro_rules! read {
+        () => ({
+            match s.get(i) {
+                None => return Err("Invalid escape"),
+                Some(c) => {
+                    i += 1;
+                    *c
+                }
+            }
+        })
+    }
+
+    macro_rules! peek {
+        () => ({
+            s.get(i).map(|c| *c)
+        })
+    }
+
+    loop {
+        if i >= s.len() {
+            return Ok(buf)
+        }
+
+        let c = read!();
+
+        if c != b'\\' {
+            buf.push(c);
+            continue
+        }
+
+        let marker = read!();
+
+        match marker {
+            b'\n' => (),
+            b'\\' => buf.push(b'\\'),
+            b'\'' => buf.push(b'\''),
+            b'"' => buf.push(b'"'),
+            b'a' => buf.push(b'\x07'),
+            b'b' => buf.push(b'\x08'),
+            b'f' => buf.push(b'\x0c'),
+            b'n' => buf.push(b'\n'),
+            b'r' => buf.push(b'\r'),
+            b't' => buf.push(b'\t'),
+            b'v' => buf.push(b'\x0b'),
+            b'x' => {
+                let first = try!(hex_to_digit(read!()));
+                let second = try!(hex_to_digit(read!()));
+                buf.push(first * 15 + second)
+            }
+            b'0' ... b'7' => {
+                oct_buf.push_front(try!(oct_to_digit(marker)));
+                peek!().and_then(|c| oct_to_digit(c).ok()).map(|s| {
+                    oct_buf.push_front(s);
+                    i += 1;
+
+                    peek!().and_then(|c| oct_to_digit(c).ok()).map(|s| {
+                        oct_buf.push_front(s);
+                        i += 1;
+                    });
+                });
+                let value = oct_buf.iter().enumerate().fold(0u16, |acc, (i, &v)| acc + v as u16 * (8u16.pow(i as u32)));
+                oct_buf.clear();
+                buf.push(if value > 255 {
+                    255
+                } else {
+                    value as u8
+                });
+                continue
+            },
+            _ => {
+                buf.push(b'\\');
+                buf.push(marker);
+            }
+        }
+    }
+}
+
 pub fn read_opcode<R>(rd: &mut R) -> Result<OpCode, Error> where R: Read + BufRead {
 
     macro_rules! ensure_not_negative {
@@ -308,7 +408,7 @@ mod tests {
 
     use num::{FromPrimitive};
 
-    use super::{OpCode, Error, read_opcode};
+    use super::{OpCode, Error, read_opcode, unescape};
 
     macro_rules! t {
         ($buffer: expr, $pat:pat, $result:expr) => ({
@@ -682,5 +782,17 @@ mod tests {
     #[test]
     fn test_bin_persid() {
         t!(b"Q", OpCode::BinPersId, ())
+    }
+
+    #[test]
+    fn test_unescape() {
+        assert_eq!(unescape(b"foo").unwrap(), b"foo");
+        assert_eq!(unescape(b"f\\noo").unwrap(), b"f\noo");
+        assert_eq!(unescape(b"f\\x01oo").unwrap(), b"f\x01oo");
+        assert_eq!(unescape(b"f\\375oo").unwrap(), b"f\xfdoo");
+        assert_eq!(unescape(b"f\\75oo").unwrap(), b"f\x3doo");
+        assert_eq!(unescape(b"f\\5oo").unwrap(), b"f\x05oo");
+        assert_eq!(unescape(b"f\\oo").unwrap(), b"f\\oo");
+        assert_eq!(unescape(b"f\\coo").unwrap(), b"f\\coo");
     }
 }
