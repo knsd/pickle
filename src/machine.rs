188fcf6e31d49131fec7e7901b2891a73b8d0b55
Fedor Gogolev <knsd@knsd.net>
2016-01-09 08:02:06 +0300

    Add pickle machine errrors

--- src/machine.rs
+++ src/machine.rs
@@ -5,6 +5,18 @@ use num::bigint::{BigInt};
 use opcode::{OpCode, BooleanOrInt};
 use value::{Value};
 
+quick_error! {
+    #[derive(Debug)]
+    pub enum Error {
+        EmptyMarker
+        StackTooSmall
+        EmptyStack
+        InvalidValueOnStack
+        InvalidMemoValue
+        NotImplemented
+    }
+}
+
 pub struct Machine {
     stack: Vec<Value>,
     memo: HashMap<usize, Value>,
@@ -20,43 +32,45 @@ impl Machine {
         }
     }
 
-    fn split_off(&mut self) -> Vec<Value> {
+    fn split_off(&mut self) -> Result<Vec<Value>, Error> {
         let at = match self.marker {
-            None => panic!("Empty marker"),
+            None => return Err(Error::EmptyMarker),
             Some(marker) => marker,
         };
 
         if at > self.stack.len() {
-            panic!("Stack too small");
+            return Err(Error::StackTooSmall);
         }
 
-        self.stack.split_off(at)
+        Ok(self.stack.split_off(at))
     }
 
-    fn pop(&mut self) -> Value {
+    fn pop(&mut self) -> Result<Value, Error> {
         match self.stack.pop() {
-            None => panic!("Empty stack"),
-            Some(value) => value,
+            None => return Err(Error::EmptyStack),
+            Some(value) => Ok(value),
         }
     }
 
-    fn handle_get(&mut self, i: usize) {
+    fn handle_get(&mut self, i: usize) -> Result<(), Error> {
         let value = match self.memo.get(&i) {
-            None => panic!("Empty memo"),
+            None => return Err(Error::InvalidMemoValue),
             Some(ref v) => (*v).clone(),
         };
         self.stack.push(value);
+        Ok(())
     }
 
-    fn handle_put(&mut self, i: usize) {
+    fn handle_put(&mut self, i: usize) -> Result<(), Error> {
         let value = match self.stack.last() {
-            None => panic!("Empty stack"),
+            None => return Err(Error::EmptyStack),
             Some(ref v) => (*v).clone(),
         };
         self.memo.insert(i, value);
+        Ok(())
     }
 
-    fn execute(&mut self, opcode: OpCode) {
+    pub fn execute(&mut self, opcode: OpCode) -> Result<(), Error> {
         match opcode {
             OpCode::Proto(_) => (),
             OpCode::Stop => (),  // TODO: !!!
@@ -91,52 +105,52 @@ impl Machine {
 
             OpCode::EmptyList => self.stack.push(Value::List(Vec::new())),
             OpCode::Append => {
-                let v = self.pop();
+                let v = try!(self.pop());
                 match self.stack.last_mut() {
-                    None => panic!("Empty stack"),
+                    None => return Err(Error::EmptyStack),
                     Some(&mut Value::List(ref mut list)) => list.push(v),
-                    _ => panic!("Invalid value on stack"),
+                    _ => return Err(Error::InvalidValueOnStack),
                 }
             },
             OpCode::Appends => {
-                let values = self.split_off();
+                let values = try!(self.split_off());
                 match self.stack.last_mut() {
-                    None => panic!("Empty stack"),
+                    None => return Err(Error::EmptyStack),
                     Some(&mut Value::List(ref mut list)) => {
                         list.extend(values);
                     },
-                    _ => panic!("Invalid value on stack"),
+                    _ => return Err(Error::InvalidValueOnStack),
                 }
             },
             OpCode::List => {
-                let values = self.split_off();
+                let values = try!(self.split_off());
                 self.stack.push(Value::List(values));
             },
 
             OpCode::EmptyTuple => self.stack.push(Value::Tuple(Vec::new())),
             OpCode::Tuple => {
-                let values = self.split_off();
+                let values = try!(self.split_off());
                 self.stack.push(Value::Tuple(values));
             },
             OpCode::Tuple1 => {
-                let v1 = self.pop();
+                let v1 = try!(self.pop());
                 self.stack.push(Value::Tuple(vec![v1]))
             },
             OpCode::Tuple2 => {
-                let v1 = self.pop();
-                let v2 = self.pop();
+                let v1 = try!(self.pop());
+                let v2 = try!(self.pop());
                 self.stack.push(Value::Tuple(vec![v1, v2]))
             },
             OpCode::Tuple3 => {
-                let v1 = self.pop();
-                let v2 = self.pop();
-                let v3 = self.pop();
+                let v1 = try!(self.pop());
+                let v2 = try!(self.pop());
+                let v3 = try!(self.pop());
                 self.stack.push(Value::Tuple(vec![v1, v2, v3]))
             }
 
             OpCode::EmptyDict => self.stack.push(Value::Dict(Vec::new())),
             OpCode::Dict => {
-                let mut values = self.split_off();
+                let mut values = try!(self.split_off());
                 let mut dict = Vec::new();
 
                 for i in 0 .. values.len() / 2 { // TODO: Check panic
@@ -147,19 +161,19 @@ impl Machine {
                 self.stack.push(Value::Dict(dict));
             },
             OpCode::SetItem => {
-                let value = self.pop();
-                let key = self.pop();
+                let value = try!(self.pop());
+                let key = try!(self.pop());
                 match self.stack.last_mut() {
-                    None => panic!("Empty stack"),
+                    None => return Err(Error::EmptyStack),
                     Some(&mut Value::Dict(ref mut dict)) => dict.push((key, value)),
-                    _ => panic!("Invalid value on stack"),
+                    _ => return Err(Error::InvalidValueOnStack),
                 }
             },
             OpCode::SetItems => {
-                let mut values = self.split_off();
+                let mut values = try!(self.split_off());
 
                 match self.stack.last_mut() {
-                    None => panic!("Empty stack"),
+                    None => return Err(Error::EmptyStack),
                     Some(&mut Value::Dict(ref mut dict)) => {
                         for i in 0 .. values.len() / 2 { // TODO: Check panic
                             let key = values.remove(2 * i);
@@ -167,16 +181,16 @@ impl Machine {
                             dict.push((key, value));
                         }
                     },
-                    _ => panic!("Invalid value on stack"),
+                    _ => return Err(Error::InvalidValueOnStack),
                 }
             },
 
             OpCode::Pop => {
-                self.pop();
+                try!(self.pop());
             },
             OpCode::Dup => {
                 let value = match self.stack.last() {
-                    None => panic!("Empty stack"),
+                    None => return Err(Error::EmptyStack),
                     Some(ref v) => (*v).clone(),
                 };
                 self.stack.push(value)
@@ -185,17 +199,18 @@ impl Machine {
                 self.marker = Some(self.stack.len())
             },
             OpCode::PopMark => {
-                self.split_off();
+                try!(self.split_off());
             },
 
-            OpCode::Get(i) => self.handle_get(i),
-            OpCode::BinGet(i) => self.handle_get(i),
-            OpCode::LongBinGet(i) => self.handle_get(i),
-            OpCode::Put(i) => self.handle_put(i),
-            OpCode::BinPut(i) => self.handle_put(i),
-            OpCode::LongBinPut(i) => self.handle_put(i),
+            OpCode::Get(i) => try!(self.handle_get(i)),
+            OpCode::BinGet(i) => try!(self.handle_get(i)),
+            OpCode::LongBinGet(i) => try!(self.handle_get(i)),
+            OpCode::Put(i) => try!(self.handle_put(i)),
+            OpCode::BinPut(i) => try!(self.handle_put(i)),
+            OpCode::LongBinPut(i) => try!(self.handle_put(i)),
 
-            _ => panic!("Not implemented")
+            _ => return Err(Error::NotImplemented)
         }
+        Ok(())
     }
 }
