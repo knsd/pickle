7f3b0d773a3428cd7a9b6d8e7aec62d710e25b61
Fedor Gogolev <knsd@knsd.net>
2016-01-14 19:26:08 +0300

    Add simple test and example

---
+++ examples/simple.rs
@@ -0,0 +1,13 @@
+#![feature(test)]
+
+use std::io::{Cursor};
+
+extern crate test;
+extern crate pickle;
+
+fn main() {
+    let buffer = include_bytes!("../pickle");
+
+    println!("{:?}", pickle::unpickle(&mut Cursor::new(&buffer[..])));
+    ()
+}
